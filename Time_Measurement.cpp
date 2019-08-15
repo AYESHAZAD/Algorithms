4d6616d4d33c24f906ddf2e9787775fb41f0dd9a
AYESHAZAD <39938138+AYESHAZAD@users.noreply.github.com>
2019-08-14 22:58:53 -0700

    Create Time_Measurement.cpp

---
+++ Time_Measurement.cpp
@@ -0,0 +1,25 @@
+/* Credit: Raihan Chowdhury
+https://github.com/raihan02
+*/
+//Measuring the execution time of an algorithm or function
+
+#include <bits/stdc++.h>
+#include <sys/time.h>
+double mysecond()
+{
+    struct timeval tp;
+    struct timezone tzp;
+    int i;
+
+    i = gettimeofday(&tp,&tzp);
+    return ( (double) tp.tv_sec + (double) tp.tv_usec * 1.e-6);
+}
+int main()
+{
+   double start_, end_;
+   start_ = mysecond();
+   //fun()
+   end_ = mysecond();
+   cout<<"Final Time = " << end_ - start << endl;
+
+}
